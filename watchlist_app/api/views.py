152350b48a1cf9569983a3648a4ae68e7e5c99aa
Robert <rcoffie22@yahoo.com>
2021-09-25 11:17:43 +0000

    put and delete view

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view 
 from watchlist_app.models import * 
 from . serializers import *
+from rest_framework import status 
 
 
 
@@ -23,8 +24,27 @@ def Movie_List(request):
 
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def Movie_Detail(request, pk):
-  movie = Movie.objects.get(pk=pk)
-  serializer = MovieSerializer(movie)
-  return Response (serializer.data)
+  if request.method == 'GET':
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie)
+    return Response (serializer.data)
+  
+  if request.method == 'PUT':
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie, data=request.data)
+    if serializer.is_valid():
+      serializer.save() 
+      return Response(serializer.data)
+    else:
+      return Response (serializer.errors)
+    
+    
+  if request.method == 'DELETE':
+    movie = Movie.objects.get(pk=pk)
+    movie.delete()
+    content = {'please movie ': 'deleted'}
+    return Response(content, status=status.HTTP_404_NOT_FOUND)
+      
+    
